5b7e1f08fc283d936ea7753d79f995ff6880cf1b
Gayazov Ilnaz <gayazov02@inbox.ru>
2023-10-25 23:22:37 +0300

    variant with get

--- src/main/java/myApplication/app1/controllers/HelloController.java
+++ src/main/java/myApplication/app1/controllers/HelloController.java
@@ -3,13 +3,13 @@ package myApplication.app1.controllers;
 import myApplication.app1.model.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public ResponseEntity<Message> hello(){
 
         Message message = new Message();
